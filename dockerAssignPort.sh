9bbb6505bf398a30014b70b278e24c1ad41e2c41
Max <max@maxf.net>
2023-12-04 18:43:51 -0500

    updated

--- dockerAssignPort.sh
+++ dockerAssignPort.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -ex
 
-src=$1
-dst=$2
+srcPort=$1
+dstPort=$2
 
-if [ -z $dst ]; then
-	echo "Usage: $0 <src> <dst>"
-	echo " e.g.: $0 80 50080"
-	exit 1
+if [ -z $dstPort ]; then
+        echo "Usage: $0 <src port> <dst port>"
+        echo " e.g.: $0 80 50080"
+        exit 1
 fi
 
-/usr/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $src -j REDIRECT --to-port $dst
+redirect="-p tcp --dport $srcPort -j REDIRECT --to-port $dstPort"
+
+/sbin/iptables -t nat -C OUTPUT -o lo $redirect || /sbin/iptables -t nat -A OUTPUT -o lo $redirect
+/sbin/iptables -t nat -C PREROUTING $redirect || /sbin/iptables -t nat -A PREROUTING $redirect
+
