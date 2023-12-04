de6f27eb293f48bb8824a4b3a632f3a3cd5ada72
Max <max@maxf.net>
2023-12-04 17:16:18 -0500

    updated

--- dockerInDockerCompose.sh
+++ dockerInDockerCompose.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose "$@"
+DOCKER_MNT=${DOCKER_MNT:-$PWD}
+envFile=/tmp/$(basename $0).$$
+env > $envFile
+docker run --rm --env-file "$envFile" -v /var/run/docker.sock:/var/run/docker.sock -v "$DOCKER_MNT:$DOCKER_MNT" -w="$PWD" docker "$@"
+rm $envFile
+
