7792d05c71691b8b040fa6aff18cfdacec54f2b3
Max <max@maxf.net>
2023-12-05 00:43:03 -0500

    updated

--- dockerAssignPort.sh
+++ dockerAssignPort.sh
@@ -14,6 +14,6 @@ $SWD/dockerUnassignPort.sh $srcPort
 
 redirect="-p tcp --dport $srcPort -j REDIRECT --to-port $dstPort -m comment --comment dpsrv:redirect:port:$srcPort"
 
-/sbin/iptables -t nat -A OUTPUT -o lo $redirect
 /sbin/iptables -t nat -A PREROUTING $redirect
+/sbin/iptables -t nat -A OUTPUT -o lo $redirect
 
