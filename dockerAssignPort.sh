a18a9a6f3c6740ddacdfa012b9cab3a4a4f8985d
Max <max@maxf.net>
2023-12-05 11:20:14 -0500

    updated

--- dockerAssignPort.sh
+++ dockerAssignPort.sh
@@ -4,16 +4,19 @@ srcPort=$1
 dstPort=$2
 
 if [ -z $dstPort ]; then
-        echo "Usage: $0 <src port> <dst port>"
-        echo " e.g.: $0 80 50080"
-        exit 1
+	echo "Usage: $0 <src port> <dst port>"
+	echo " e.g.: $0 80 50080"
+	exit 1
 fi
 
 SWD=$(cd $(dirname $0); pwd)
 $SWD/dockerUnassignPort.sh $srcPort
 
-redirect="-p tcp --dport $srcPort -j REDIRECT --to-port $dstPort -m comment --comment dpsrv:redirect:port:$srcPort"
+redirect="-t nat -p tcp --dport $srcPort -j REDIRECT --to-port $dstPort -m comment --comment dpsrv:redirect:port:$srcPort"
+accept="-A INPUT -m comment --comment dpsrv:redirect:port:$srcPort -p tcp -j ACCEPT --dport"
 
-/sbin/iptables -t nat -A PREROUTING $redirect
-/sbin/iptables -t nat -A OUTPUT -o lo $redirect
+/sbin/iptables $accept $srcPort
+/sbin/iptables $accept $dstPort
+/sbin/iptables -A PREROUTING $redirect
+/sbin/iptables -A OUTPUT -o lo $redirect
 
--- dockerUnassignPort.sh
+++ dockerUnassignPort.sh
@@ -1,16 +1,22 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 srcPort=$1
 
 if [ -z $srcPort ]; then
-        echo "Usage: $0 <src port>"
-        echo " e.g.: $0 80"
-        exit 1
+	echo "Usage: $0 <src port>"
+	echo " e.g.: $0 80"
+	exit 1
 fi
 
 comment="dpsrv:redirect:port:$srcPort"
 
-/sbin/iptables-save | grep -- "$comment" | sed 's/^-A/-D/g' | while read line; do
-        echo $line | xargs /sbin/iptables -t nat
+/sbin/iptables-save | while read line; do
+	if [[ $line =~ ^\*(.*) ]]; then
+		table=${BASH_REMATCH[1]}
+		continue
+	fi
+	command=$(echo "$line" | grep -- "$comment" | sed 's/^-A/-D/g')
+	[ -n "$command" ] || continue
+	echo $command | xargs /sbin/iptables -t $table
 done
 
